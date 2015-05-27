2e6584639450e0c264665468500920b41a1ab3b2
Nedearb <nedearb@gmail.com>
2015-05-27 14:35:48 -0700

    update

---
+++ globaImpots.js
@@ -0,0 +1,19 @@
+
+
+function read(source, onload){
+    var xhr=new XMLHttpRequest;
+    xhr.open('GET', source);
+    xhr.onload = onload;
+    xhr.send();
+}
+
+
+function initImports(){
+    var header = document.getElementById("");
+    
+    read("/header.html", function(){
+        var div = document.getElementById("page_header");
+        div.innerHTML = this.response+div.innerHTML;
+    });
+    
+}
