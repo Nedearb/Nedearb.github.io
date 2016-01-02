fcfa746a8220b4c89ff5b9422ea9e7a77fc6cd9e
Nedearb <nedearb@gmail.com>
2016-01-01 21:33:31 -0700

    test

--- globaImpots.js
+++ globaImpots.js
@@ -12,7 +12,7 @@ function initImports(){
     var header = document.getElementById("");
     
     read("/header.html", function(){
-        var div = document.getElementById("page_header");
+        var div = document.getElementById("page_wrapper");
         div.innerHTML = this.response+div.innerHTML;
     });
     
