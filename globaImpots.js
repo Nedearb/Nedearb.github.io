c70e1bd5b93d41f124f123eeedd0dd05f60749de
Braeden Atlee <nedearb@gmail.com>
2016-05-20 17:56:27 -0700

    simplify

--- globaImpots.js
+++ globaImpots.js
@@ -1,26 +1,20 @@
 
 
 function read(source, onload){
-    var xhr=new XMLHttpRequest;
-    xhr.open('GET', source);
-    xhr.onload = onload;
-    xhr.send();
-}
+  var xhr=new XMLHttpRequest;
+  xhr.open('GET', source);
+  xhr.onload = onload;
+  xhr.send();
+};
 
 
 function initImports(){
-    var header = document.getElementById("");
-    
-    read("/header.html", function(){
-        var div = document.getElementById("page_wrapper");
-        div.innerHTML = this.response+div.innerHTML;
-    });
-    
-    /*read("/footer.html", function(){
-        var div = document.getElementById("page_wrapper");
-        div.innerHTML = div.innerHTML+this.response;
-    });*/
-    
-}
+  
+  read("/header.html", function(){
+      var div = document.getElementById("page_wrapper");
+      div.innerHTML = this.response+div.innerHTML;
+  });
+  
+};
 
 initImports();
